076c137149adfe302f047646912adb143815776a
felipe.nascimento.ecommet@gmail.com <felipe.nascimento.ecommet@gmail.com>
2018-04-04 13:26:37 -0300

    Adicionado exemplos para typescript.

--- angular-4/typescript/app.ts
+++ angular-4/typescript/app.ts
@@ -10,9 +10,18 @@ let favoriteDroid: string
 favoriteDroid = 'BB-8'
 console.log("My favorite droid is " + favoriteDroid)
 
-let isEnoughToBeatMF = function(parsecs: number) : boolean {
+let isEnoughToBeatMF = function(parsecs: number): boolean {
 	return parsecs < 12
 }
 
-let distance = 10
+let distance = 14
 console.log(`Is ${distance} parsecs enough to beat Millennium Falcon? ${isEnoughToBeatMF(distance) ? 'YES' : 'NO'}`)
+
+let call = (name: string) => console.log(`Do you copy, ${name}?`)
+call('R2')
+
+function inc(speed: number, inc: number = 1): number {
+	return speed + inc
+}
+console.log(`inc(5, 1) = ${inc(5, 1)}`)
+console.log(`inc(5) = ${inc(5)}`)
